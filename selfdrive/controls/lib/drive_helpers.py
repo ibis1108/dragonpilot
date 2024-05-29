917bc0fc0ddf8bda6ac2f09bc812d8c54ef78d76
ibis1108 <149675206+ibis1108@users.noreply.github.com>
2024-05-29 20:42:47 +0900

    Update drive_helpers.py

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -13,7 +13,7 @@ V_CRUISE_MIN = 8
 V_CRUISE_MAX = 145
 V_CRUISE_UNSET = 255
 V_CRUISE_INITIAL = 10
-V_CRUISE_INITIAL_EXPERIMENTAL_MODE = 55
+V_CRUISE_INITIAL_EXPERIMENTAL_MODE = 10
 IMPERIAL_INCREMENT = 1.6  # should be CV.MPH_TO_KPH, but this causes rounding errors
 
 MIN_SPEED = 1.0
@@ -32,7 +32,7 @@ CRUISE_NEAREST_FUNC = {
   ButtonType.decelCruise: math.floor,
 }
 CRUISE_INTERVAL_SIGN = {
-  ButtonType.accelCruise: +1,
+  ButtonType.accelCruise: +5,
   ButtonType.decelCruise: -1,
 }
 
